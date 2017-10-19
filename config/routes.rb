e6affec481b651fc6b3a72af61fe435b880a512f
Hoseong Hwang <thefron@42.company>
2017-10-20 00:31:01 +0900

    Add basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :v1, defaults: { format: :json } do
+    devise_for :users
+    resources :lectures, only: [:index, :show] do
+      resources :evaluations, only: [:index, :create, :update, :destroy]
+    end
+  end
 end
