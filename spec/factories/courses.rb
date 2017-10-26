4d120b72137bf0dd5b44ec67f7034517c480d24f
Hoseong Hwang <thefron@42.company>
2017-10-27 02:26:30 +0900

    Rename factory_girl -> factory_bot

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -1,4 +1,4 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :course do
     association :department
     sequence(:name) { |n| "Course-#{n}" }
--- spec/factories/evaluations.rb
+++ spec/factories/evaluations.rb
@@ -1,4 +1,4 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :evaluation do
     association :lecture
     association :user
