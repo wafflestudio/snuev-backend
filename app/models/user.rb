941c9ac25fcec8b938ac9dab52cdfdfdee9648c9
Hoseong Hwang <thefron@42.company>
2017-11-01 02:09:49 +0900

    Add user methods regarding email confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,25 @@ class User < ApplicationRecord
   has_many :evaluations
 
   before_validation :set_email_from_username, on: :create
+  before_validation :set_confirmation_token, on: :create
 
   validates_presence_of :username, :email, :password_digest
+
+  def confirmed?
+    confirmed_at.present?
+  end
+
+  def confirm_email!
+    update!(confirmed_at: DateTime.current, confirmation_token: nil)
+  end
 
   private
 
   def set_email_from_username
     self.email ||= "#{username}@snu.ac.kr"
+  end
+
+  def set_confirmation_token
+    self.confirmation_token = SecureRandom.urlsafe_base64.to_s
   end
 end
