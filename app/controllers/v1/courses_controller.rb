478f926befb3b4783a10ede68a52e35693ff0f43
Taekmin Kim <tantara.tm@gmail.com>
2017-11-12 21:30:11 +0900

    Remove remaining jbuilder

--- app/controllers/v1/courses_controller.rb
+++ app/controllers/v1/courses_controller.rb
@@ -1,7 +1,6 @@
-class V1::CoursesController < ApplicationController
-#class V1::CoursesController < V1::BaseController
+class V1::CoursesController < V1::BaseController
   def index
-
    	@courses = SearchIndex::Course.query(match: {name: params[:q]}).objects
+    render jsonapi: @courses
   end
 end
--- app/views/v1/courses/index.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.courses(@courses, :id, :name)
