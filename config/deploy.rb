d2c765b87fb5a3c44383aaf8e19e24e32d0f282d
Hoseong Hwang <thefron@42.company>
2018-06-24 18:21:29 +0900

    Remove precompile process

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,7 +54,6 @@ task :deploy do
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
-    invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
     on :launch do
