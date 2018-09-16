02aa4bfe9f2f9dc305f998ffe2153c4db7d54bb1
Hoseong Hwang <liam.hwang@buzzvil.com>
2018-09-16 21:26:34 +0900

    Allow nil for evaluation user per lecture

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -10,7 +10,7 @@ class Evaluation < ApplicationRecord
   validates :score, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10, only_integer: true }
   validates :easiness, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10, only_integer: true }
   validates :grading, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10, only_integer: true }
-  validates :user, uniqueness: { scope: :lecture }
+  validates :user, uniqueness: { scope: :lecture, allow_nil: true }
 
   before_validation :set_default_semester
   after_save :update_lecture_scores
